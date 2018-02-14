00d318abbc92a8fe650016ba91ae62bef97cb936
Dheeraj Pal <paldheeraj25@gmail.com>
2018-02-14 12:11:14 +0530

    bounty function implemented

--- contracts/Elira.sol
+++ contracts/Elira.sol
@@ -10,20 +10,25 @@ import "./StandardToken.sol";
  * Note they can later distribute these tokens as they wish using `transfer` and other
  * `StandardToken` functions.
  */
-contract Elira is StandardToken {
+contract Elira1 is StandardToken {
 
   string public constant name = "Elira Token"; // solium-disable-line uppercase
   string public constant symbol = "ELIRA"; // solium-disable-line uppercase
   uint8 public constant decimals = 18; // solium-disable-line uppercase
   uint public constant rate = 1000;// amount of token per ether
+  uint public etherCap = 800000;//ether cap to raise
+  
   address private _owner;
 
-  uint256 public constant INITIAL_SUPPLY = 8000000000;
+  uint256 public constant INITIAL_SUPPLY = 600000000;
+  uint public bountyAllocation = 200000000;
 
+  event Bounty(address _address, uint _value);
+  
   /**
    * @dev Constructor that gives msg.sender all of existing tokens.
    */
-  function Elira() public {
+  function Elira1() public {
     totalSupply_ = INITIAL_SUPPLY;
     _owner = msg.sender;
     Transfer(0x0, msg.sender, INITIAL_SUPPLY);
@@ -42,4 +47,21 @@ contract Elira is StandardToken {
   function () public payable {
       createToken();
   }
+  
+  /**
+   * @dev Transfer bouty tokens on signup
+   * @param _value uint256 the amount of tokens to be transferred
+   */
+  function bounty(uint _value) public {
+      require(_value == 5 || _value == 10);
+      //check bounty funds
+      require(bountyAllocation > _value);
+      // add bounty count to thier account
+      balances[msg.sender] = balances[msg.sender].add(_value);
+      // substract bouty from bounty fund
+      bountyAllocation = bountyAllocation.sub(_value);
+      Bounty(msg.sender, _value);
+  }
+  
+
 }
